62f3ae79690ad52bc1a5a91a5cfb8487efbb38e9
Anya Kovaleva <annkov007@gmail.com>
2023-09-16 23:48:08 +0300

    class for energy cat variants

---
+++ Assets/Scripts/Collection/EnergyCat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Collection
+{
+    [Serializable]
+    public class EnergyCat
+    {
+        public Sprite Image;
+        public string Name;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/EnergyCatsSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Collection;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu]
+    public class EnergyCatsSO : ScriptableObject
+    {
+        public List<EnergyCat> EnergyCats;
+    }
+}
